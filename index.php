8ab729cc88c0ec373d02041b1b22a8c8c23d4422
Chocogithub <martinbacud@gmail.com>
2022-08-21 20:48:09 +0800

    first file upload

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Chocotapz Personal Site</title>
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+
+		<table cellspacing="20">
+			<tr>
+				<td><img src="img/tapz.png" style="width:200px"></td>
+				<td><h1>Martin Tapang</h1>
+				<p><em>Founder and CEO of Choco Corp.</em></p>
+				<p><em>Learning CSS, HTML and Javascript for the future 🐱‍👤🐱‍👤</em></p> </td>
+			</tr>
+		</table>
+		
+		<hr>
+		<h3>Work Experience</h3>
+			<ul>
+				<li>Web Developer someday</li>
+				<li><a href="https://resources.workable.com/computer-technician-job-description"> Computer Tech</a></li>
+				<li><a href="https://techsupport.ph/">Tech Support</a></li>
+			</ul>
+		<hr>
+		<h3>Skills</h3>
+		<table cellspacing="5">
+			<tr>
+				<td>PHP ⭐⭐⭐</td>
+			</tr>
+			<tr>
+				<td>Java ⭐⭐</td>
+			</tr>
+			<tr>
+				<td>CSS ⭐⭐⭐</td>
+			</tr>
+			<tr>
+				<td>Javasript ⭐⭐</td>
+			</tr>
+			<tr>
+				<td>Computer Troubleshooting ⭐⭐⭐</td>
+			</tr>
+			<tr>
+				<td>Networking ⭐⭐⭐</td>
+			</tr>
+			<tr>
+				<td>CCTV setup & maintenance ⭐⭐⭐</td>
+			</tr>
+		</table>
+	</body>
+</html>
